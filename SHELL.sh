f531e29035c3bcba101b8bd45d7828f392a29964
jmnohales <jmnohales@gmail.com>
2017-08-02 20:20:46 +0200

    first commit

---
+++ SHELL.sh
@@ -0,0 +1,9 @@
+
+## EXAMPLE ansible-playbook -i inventary/node2 -e "node_hostname=node2.example.demo" ./hostname.yml
+
+hostname.yml
+nmcli_dhcp_to_static_IP.yml
+rhel_subscribe.yml
+ocp_repos.yml
+ocp_package_prepare.yml
+ocp_docker_package.yml
