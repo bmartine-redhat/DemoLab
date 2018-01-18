d1c7ef4c22e71f2012066c4eaa2f78926127c2c2
jmnohales <jmnohales@gmail.com>
2018-01-18 16:22:29 +0100

    Delete SHELL_EXAMPLE.sh

--- SHELL_EXAMPLE.sh
+++
@@ -1,3 +0,0 @@
-
-## EXAMPLE ansible-playbook -i inventary/node2 -e "node_hostname=node2.example.demo" ./hostname.yml
-ansible-playbook -i "192.168.121.242," -e "node_hostname=testhost.example.demo" ./hostname.yml
